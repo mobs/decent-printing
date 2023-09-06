74b21054b03745914b7aeb5cc05cf56f54423b4e
Mobashir Alam <mobashira3@gmail.com>
2023-09-06 15:05:55 +0530

    updated env and package files

--- server/index.js
+++ server/index.js
@@ -7,15 +7,10 @@ import { config } from "dotenv";
 import productRoute from "./routes/product.js";
 import userRoute from "./routes/user.js";
 import galleryRoute from './routes/gallery.js'
-// import cartRoute from "./routes/cart.js";
 
 config({ path: "./config/config.env" });
 const app = express();
 
-// export const instance = new Razorpay({
-//   key_id: process.env.RAZORPAY_API_KEY,
-//   key_secret: process.env.RAZORPAY_API_SECRET,
-// });
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -24,9 +19,7 @@ app.use(cors());
 app.use("/product", productRoute);
 app.use("/user", userRoute);
 app.use('/gallery', galleryRoute);
-// // app.use("/banner", bannerRoute);
-// // app.use("/payment", paymentRoute);
-// app.use("/cart", cartRoute);
+
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
